09ac73fa9d16c17eb9b6800d5e65204c45df33e2
adityabanstola <58278493+adityabanstola@users.noreply.github.com>
2022-01-06 16:31:28 +0545

    Delete lab 2 questions directory

--- lab 2 questions/question-1/UserDefinedPackage1.java
+++
@@ -1,9 +0,0 @@
-//WAP to demonstrate the use of user defined package in java.
-package MyPackage;
-
-public class UserDefinedPackage1
-{
-    public static void sayHello(String Name){
-        System.out.println("Hello " + Name);
-    }
-}
--- lab 2 questions/question-2/MyException.java
+++
@@ -1,9 +0,0 @@
-import java.lang.Exception;
-
-class MyException extends Exception
-{
-    public MyException()
-    {
-        super("This is my exception");
-    }
-}
